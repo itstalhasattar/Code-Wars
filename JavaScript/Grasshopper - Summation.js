8c6dffd4e1d97d6190dabe57d76a50a0437b0c88
Talha Sattar <talhasattar@Talhas-MacBook-Air.local>
2023-06-05 20:32:53 -0400

    New Kata

---
+++ JavaScript/Grasshopper - Summation.js
@@ -0,0 +1,10 @@
+var summation = function (num) {
+    // Code here
+    let result= 0;
+    let counter=0;
+    while (counter <= num){
+      result=result + counter
+      counter = counter+1
+    }
+    return result
+  }
